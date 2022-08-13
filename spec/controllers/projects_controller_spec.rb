eb49c80dfaf4c6e6b95bfc20e89ab6eec10a8628
ooooose <yuuki.oose1990@gmail.com>
2022-08-13 13:55:07 +0900

    Keep DRY

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -13,15 +13,11 @@ RSpec.describe ProjectsController, type: :controller do
       it "responds successfully" do
         sign_in @user
         get :index
-        expect(response).to be_successful
+        aggregate_failures do
+          expect(response).to be_successful
+          expect(response).to have_http_status "200"
+        end
       end 
-    
-      # 200レスポンスを返すこと
-      it "returns a 200 response" do
-        sign_in @user
-        get :index
-        expect(response).to have_http_status "200"
-      end
     end
 
     # ゲストとして
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,5 @@ RSpec.configure do |config|
   # コントローラスペックでDeviseのテストヘルパーを使用する
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include RequestSpecHelper, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
